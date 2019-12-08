6e1f02c5f5a5655812b56b5fa0639da10698c62c
Robert Woolley <rob@robertwoolley.co.uk>
2019-12-08 19:22:24 +0000

    Basic posts and views in place

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,21 @@
 class PostsController < ApplicationController
-end
 
-def index
-end
+  def index
+    @posts = Post.all
+  end
 
-def new
-  @post = Post.new
-end
+  def new
+    @post = Post.new
+  end
 
-def create
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
 end
