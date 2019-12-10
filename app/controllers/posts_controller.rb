ce1118665677061e7517a1adfd20544daa50d83d
Robert Woolley <rob@robertwoolley.co.uk>
2019-12-10 12:17:04 +0000

    Updates for edit/modify

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,16 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))
   end
 
   private
